ce5131ec94042e706cdd2b8e034fd5467df4d4dd
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-09-20 23:37:13 +0200

    added post content styling and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Link from "next/link";
+import PaddingContainer from "@/components/layout/paddingContainer";
+import PostCard from "@/components/post/postCard";
+import { DUMMY_POSTS } from "@/DUMMY_DATA";
 
 export default function Home() {
   return (
-    <main className="h-auto">
-      <h1>Home</h1>
-    </main>
+    <PaddingContainer>
+      <main className="h-auto space-y-10">
+        <PostCard post={DUMMY_POSTS[0]} />
+      </main>
+    </PaddingContainer>
   );
 }
---
+++ components/post/postCard.tsx
@@ -0,0 +1,30 @@
+import { Post } from "@/types/collection";
+import Image from "next/image";
+import Link from "next/link";
+import PostContent from "./postContent";
+
+interface PostProps {
+  post: Post;
+}
+
+const PostCard = ({ post }: PostProps) => {
+  return (
+    <Link
+      className="grid grid-cols-2 gap-10 items-center"
+      href={`/post/${post.slug}`}
+    >
+      {/* Post Image */}
+      <Image
+        className="rounded w-full object-cover object-center max-h-[300px]"
+        alt={post.title}
+        src={post.image}
+        width={600}
+        height={300}
+      />
+      {/* Post Content */}
+      <PostContent post={post} />
+    </Link>
+  );
+};
+
+export default PostCard;
