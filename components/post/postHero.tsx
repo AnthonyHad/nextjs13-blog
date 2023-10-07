83a736e42170be1b39cc88d7d105bd0f52802537
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-10-07 11:43:56 +0200

    added post data fetching and some styling for post body

--- components/post/postHero.tsx
+++ components/post/postHero.tsx
@@ -11,7 +11,7 @@ const PostHero = ({ post }: PostHeroProp) => {
       <PostContent post={post} isPostPage />
       <Image
         className="rounded-md object-cover object-center h-[300px] md:h-[500px] mt-6"
-        src={post.image}
+        src={`${process.env.NEXT_PUBLIC_ASSETS_URL}${post.image}?key=optimised`}
         width={1200}
         height={500}
         alt={post.title}
